cc6429480f0425dc5a8f8b7dbab405dff79becaf
yamyamchaen <163889145+yamyamchaen@users.noreply.github.com>
2025-01-15 12:59:38 +0900

    Add files via upload

---
+++ Spring-Study/src/test/java/junsung/afterbasic/AfterBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package junsung.afterbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
